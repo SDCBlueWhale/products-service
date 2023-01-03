0643935661cdca8999daf205d68864f900ce68fe
Phea Duch <phea.duch@gmail.com>
2023-01-02 22:18:18 -0500

    added rest of grpc endpoints to match api

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,21 +1,53 @@
 import { createServer } from 'nice-grpc';
-import { GetRequest, GetResponse, Product, ProductsServiceDefinition, ProductsServiceImplementation } from '../gen/products.js';
+import {
+  GetRelatedRequest,
+  GetRelatedResponse,
+  GetRequest,
+  GetResponse,
+  GetStylesRequest,
+  GetStylesResponse,
+  ListRequest,
+  ListResponse,
+  Product,
+  ProductsServiceDefinition,
+  ProductsServiceImplementation,
+} from '../gen/products.js';
+
+const get = async (req: GetRequest): Promise<GetResponse> => {
+  let prod: Product = {
+    id: 1,
+    name: 'hello',
+    description: 'this is the description',
+    category: 'what about the cat',
+    defaultPrice: 100,
+    features: [],
+  };
+  return { product: prod };
+};
+
+const list = async (req: ListRequest): Promise<ListResponse> => {
+  let resp: ListResponse;
+  return resp;
+};
+
+const getStyles = async (req: GetStylesRequest): Promise<GetStylesResponse> => {
+  let resp: GetStylesResponse;
+  return resp;
+};
+
+const getRelated = async (req: GetRelatedRequest): Promise<GetRelatedResponse> => {
+  let resp: GetRelatedResponse;
+  return resp;
+};
 
 (async () => {
   const server = createServer();
 
   const productServiceImpl: ProductsServiceImplementation = {
-    async get(request: GetRequest): Promise<GetResponse> {
-      let prod: Product = {
-        id: 1,
-        name: 'hello',
-        description: 'this is the description',
-        category: 'what about the cat',
-        defaultPrice: 100,
-        features: [],
-      };
-      return { product: prod };
-    },
+    get,
+    list,
+    getStyles,
+    getRelated,
   };
   server.add(ProductsServiceDefinition, productServiceImpl);
 
