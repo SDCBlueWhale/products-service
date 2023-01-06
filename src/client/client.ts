96990d8066b2c674c95267a4dcd0d3366543788b
Phea Duch <phea.duch@gmail.com>
2023-01-06 13:25:53 -0500

    switched to fastify

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,13 +1,128 @@
-import { createChannel, createClient } from 'nice-grpc';
-import { GetRequest, ProductsServiceClient, ProductsServiceDefinition } from '../gen/products.js';
+import Fastify from 'fastify';
+import compression from '@fastify/compress';
+import postgres from '@fastify/postgres';
 
+const app = Fastify({});
 (async () => {
-  const channel = createChannel('localhost:8080');
-
-  const client: ProductsServiceClient = createClient(ProductsServiceDefinition, channel);
-
-  let req: GetRequest = { id: 1 };
-  const resp = await client.get(req);
-  console.log(resp);
-  channel.close();
+  await app.register(compression, { encodings: ['gzip', 'deflate'], global: true });
+  await app.register(postgres, {
+    connectionString: 'postgres://pd:dumb1234@localhost/sdc',
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 10000,
+    max: 20,
+  });
 })();
+
+const port = 3000;
+
+app.get('/products', async (req, res) => {
+  const query = `SELECT
+  id, name, slogan, description, category, default_price
+FROM
+  products
+WHERE
+  id > $1 AND id < $2;`;
+
+  const page = req.query['page'] || 1;
+  const count = req.query['count'] || 5;
+  const vals = [count * (page - 1), count * page + 1];
+  const db = await app.pg.connect();
+
+  try {
+    const { rows } = await db.query(query, vals);
+    return res.type('application/json').send(rows);
+  } catch (err) {
+    console.log('db error:', err);
+  } finally {
+    db.release();
+  }
+});
+
+app.get('/products/:product_id', async (req, res) => {
+  const query = `SELECT
+  products.*,
+  json_agg(cast(row(features.feature, features.value) AS feature) ORDER BY features.id)
+      AS features
+FROM
+  products
+LEFT JOIN
+  features
+ON
+  products.id = features.product_id
+WHERE
+  features.product_id = $1
+GROUP BY
+  products.id;
+`;
+  const db = await app.pg.connect();
+  try {
+    const { rows } = await db.query(query, [req.params['product_id']]);
+    return res.type('application/json').send(rows[0]);
+  } catch (err) {
+    console.log('db error:', err);
+  } finally {
+    db.release();
+  }
+});
+
+app.get('/products/:product_id/styles', async (req, res) => {
+  const query = `SELECT
+    public.styles.id AS style_id,
+    public.styles.name,
+    public.styles.sale_price,
+    public.styles.original_price,
+    public.styles.default_style,
+    photos,
+    skus
+FROM
+    public.styles
+LEFT JOIN LATERAL (
+    SELECT json_agg(cast(row(p.url, p.thumbnail_url) AS photo) ORDER BY p.id) AS photos
+    FROM photos p
+    WHERE public.styles.id = p.style_id
+) p ON TRUE
+LEFT JOIN LATERAL (
+    SELECT json_agg(cast(row(s.quantity, s.size) AS sku) ORDER BY s.id) AS skus
+    FROM public.skus s
+    WHERE public.styles.id = s.style_id
+) s ON TRUE
+WHERE
+    public.styles.product_id = $1;`;
+
+  const db = await app.pg.connect();
+  try {
+    const { rows } = await db.query(query, [req.params['product_id']]);
+    return res.type('application/json').send(rows);
+  } catch (err) {
+    console.log('db error:', err);
+  } finally {
+    db.release();
+  }
+});
+
+app.get('/products/:product_id/related', async (req, res) => {
+  const query = `SELECT ARRAY(SELECT related_product_id FROM related WHERE related.current_product_id = $1);`;
+  const db = await app.pg.connect();
+  try {
+    const { rows } = await db.query(query, [req.params['product_id']]);
+    return res.type('application/json').send(rows[0].array);
+  } catch (err) {
+    console.log('db error:', err);
+  } finally {
+    db.release();
+  }
+});
+
+const start = async () => {
+  try {
+    await app.listen({ port: port });
+  } catch (err) {
+    console.log('unable to start server:', err);
+    process.exit(1);
+  }
+};
+start();
+
+process.on('SIGTERM', () => {
+  app.close();
+});
