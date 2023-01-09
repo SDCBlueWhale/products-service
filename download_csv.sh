6e3c32a17d93ef8250d08e6c2eb7e0665c4c028b
Phea Duch <phea.duch@gmail.com>
2023-01-09 11:18:01 -0500

    expanded readme.md

---
+++ download_csv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+apk update
+apk add wget
+wget --version
+
+IDS=('1-W_7beRPqrP95t6DTpxdSCNsvn6gM8GM' '1zHrArvW4JI_K5Kj9uUyT9jFK4OLkrbNc' '11vpwszUgdEuT26QIJaswTF6z2d-m-uZK' '1RrUnymxIrJlbBDPfjVerYxixwpdQttwI' '1a8FjukI7bR4zav3MIcIETHGkEC5Oazhz' '1u-AQPr3pauwCophsmjcg-qGy7zabfXcR')
+NAMES=('products.csv' 'styles.csv' 'features.csv' 'related.csv' 'skus.csv' 'photos.csv')
+for i in "${!IDS[@]}"; do
+  wget --load-cookies cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id='"${IDS[i]}" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=${IDS[i]}" -O /tmp/${NAMES[i]} && rm -rf cookies.txt
+done
